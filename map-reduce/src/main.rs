8b3f6e13c1029956bdb78d87997c181ca003b756
Ertugrul Karademir <ekarademir@gmail.com>
2020-09-06 22:24:36 +0100

    reverted back to simple setup. oiler plate for http server

--- map-reduce/src/main.rs
+++ map-reduce/src/main.rs
@@ -1,17 +1,44 @@
-mod screen;
+use std::convert::Infallible;
+use std::error::Error;
 
+use env_logger;
+use log::{debug, info, error};
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
 
-struct MachineState {
-    logs: String,
-    buffers: String,
-    identity: String,
-    cluster: String
+async fn hello_world(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+    debug!("Responding to hello_world request");
+    Ok(
+        Response::new(
+            Body::from(
+                "Hello World!"
+            )
+        )
+    )
 }
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>>{
+    env_logger::init();
 
-fn main() {
+    let make_hello_world_svc = make_service_fn(|_conn| {
+        async {
+            Ok::<_, Infallible>(
+                service_fn(hello_world)
+            )
+        }
+    });
 
-    let screen = screen::Screen::new();
+    let addr = ([127, 0, 0, 1], 3000).into();
 
-    screen.start();
+    let server = Server::bind(&addr).serve(make_hello_world_svc);
+
+    info!("Listening on http://{}", addr);
+
+    // server.await?;
+    if let Err(e) = server.await {
+        error!("server error: {}", e);
+    }
+
+    Ok(())
 }
