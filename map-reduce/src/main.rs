247dea28c48f9b0937def8f0d96d15b5ea443603
Ertugrul Karademir <ekarademir@gmail.com>
2020-09-22 23:02:35 +0100

    Clean up

--- map-reduce/src/main.rs
+++ map-reduce/src/main.rs
@@ -1,22 +1,37 @@
-mod server;
 mod api;
 mod errors;
 
 use std::error::Error;
+use std::convert::Infallible;
 
 use env_logger;
 use log::{info, error};
+use hyper::{Server};
+use hyper::service::{make_service_fn, service_fn};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync>>{
+async fn main() {
     env_logger::init();
 
-    info!("I am a {:?} machine", server::kind());
+    let make_service = make_service_fn(|_conn| {
+        async {
+            Ok::<_, Infallible>(
+                service_fn(api::main_service)
+            )
+        }
+    });
 
-    if let Err(e) = server::serve().await {
+
+    info!("I am a {:?} machine", api::system::kind());
+    let addr = ([0, 0, 0, 0], 3000).into();
+    info!("Listening on http://{}", addr);
+
+    let server = Server::bind(&addr).serve(make_service);
+
+
+    if let Err(e) = server.await {
         error!("Problem bootstrapping the server: {}", e);
     }
-    Ok(())
 }
 
 
