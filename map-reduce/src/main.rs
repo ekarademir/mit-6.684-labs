596276f967eaad86318f03066e43abb16798333c
Ertugrul Karademir <ekarademir@gmail.com>
2020-09-23 21:04:44 +0100

    Run server in a separate thread

--- map-reduce/src/main.rs
+++ map-reduce/src/main.rs
@@ -1,38 +1,52 @@
 mod api;
 mod errors;
 
-use std::error::Error;
 use std::convert::Infallible;
+use std::thread;
 
 use env_logger;
 use log::{info, error};
 use hyper::{Server};
 use hyper::service::{make_service_fn, service_fn};
+use tokio::signal;
+use tokio::runtime::Runtime;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     env_logger::init();
 
-    let make_service = make_service_fn(|_conn| {
-        async {
-            Ok::<_, Infallible>(
-                service_fn(api::main_service)
-            )
-        }
+    let server_handler = thread::spawn(|| {
+        let mut rt = Runtime::new().unwrap();
+
+        rt.block_on(async {
+            // Make server and kick off
+            let make_service = make_service_fn(|_conn| {
+                async {
+                    Ok::<_, Infallible>(
+                        service_fn(api::main_service)
+                    )
+                }
+            });
+            info!("I am a {:?} machine", api::system::kind());
+            let addr = ([0, 0, 0, 0], 3000).into();
+            info!("Listening on http://{}", addr);
+
+            let server = Server::bind(&addr)
+                .serve(make_service)
+                .with_graceful_shutdown(async {
+                    signal::ctrl_c().await.unwrap();
+                    info!("Shutting down");
+                });
+
+            if let Err(e) = server.await {
+                error!("Problem bootstrapping the server: {}", e);
+            }
+        });
+
     });
 
+    info!("OSMAN");
 
-    info!("I am a {:?} machine", api::system::kind());
-    let addr = ([0, 0, 0, 0], 3000).into();
-    info!("Listening on http://{}", addr);
-
-    let server = Server::bind(&addr).serve(make_service);
-
-
-    if let Err(e) = server.await {
-        error!("Problem bootstrapping the server: {}", e);
-    }
+    server_handler.join().unwrap();
 }
-
 
 // TODO tests
