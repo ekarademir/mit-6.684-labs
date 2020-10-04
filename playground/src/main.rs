26ef4c15ec8de7351692e783cc1dede6758cf243
Ertugrul Karademir <ekarademir@gmail.com>
2020-10-04 11:08:24 +0100

    added hashset play around

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -19,7 +19,30 @@
 //     }
 // }
 
-use std::collections::HashMap;
+// use std::collections::HashMap;
+
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+
+#[derive(Debug)]
+struct Mine {
+    comp: String,
+    field: String,
+}
+
+impl PartialEq for Mine {
+    fn eq(&self, other: &Self) -> bool {
+        self.comp == other.comp
+    }
+}
+
+impl Eq for Mine {}
+
+impl Hash for Mine {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.comp.hash(state);
+    }
+}
 
 fn main () {
     // use std::time::Instant;
@@ -33,14 +56,39 @@ fn main () {
     // let uri = Uri::from_static("http://master:3000");
     // println!("{:?}", uri.host_port());
 
-    let mut map: HashMap<String, String> = HashMap::new();
+    // let mut map: HashMap<String, String> = HashMap::new();
 
-    println!("Begin {:?}", map);
-    map.insert("first".to_string(), "first item".to_string());
-    println!("First insert {:?}", map);
-    map.insert("second".to_string(), "second item".to_string());
-    println!("Second insert {:?}", map);
-    map.insert("first".to_string(), "first item again".to_string());
-    println!("Third insert {:?}", map);
+    // println!("Begin {:?}", map);
+    // map.insert("first".to_string(), "first item".to_string());
+    // println!("First insert {:?}", map);
+    // map.insert("second".to_string(), "second item".to_string());
+    // println!("Second insert {:?}", map);
+    // map.insert("first".to_string(), "first item again".to_string());
+    // println!("Third insert {:?}", map);
+
+
+    let mut set: HashSet<Mine> = HashSet::new();
+    println!("Begin {:?}", set);
+    set.replace(
+        Mine {
+            comp: "first".to_string(),
+            field: "First item to insert".to_string(),
+        }
+    );
+    println!("First insert {:?}", set);
+    set.replace(
+        Mine {
+            comp: "second".to_string(),
+            field: "Second item to insert".to_string(),
+        }
+    );
+    println!("Second insert {:?}", set);
+    set.replace(
+        Mine {
+            comp: "first".to_string(),
+            field: "Inserting to first item again".to_string(),
+        }
+    );
+    println!("Third insert {:?}", set);
 
 }
