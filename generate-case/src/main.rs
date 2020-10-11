c007ad1088cf3a9e24c993d68934d54a4db752d6
Ertugrul Karademir <ekarademir@gmail.com>
2020-10-11 21:33:08 +0100

    Parsing finished

--- generate-case/src/main.rs
+++ generate-case/src/main.rs
@@ -10,8 +10,7 @@ fn main () {
     env_logger::try_init().expect("Can't initialize logger");
 
     let mut path = env::current_dir().unwrap();
-    // path.push("./generate-case/data/oxford_5000.txt");
-    path.push("./generate-case/data/oxford_5000_all.txt");
+    path.push("./generate-case/data/words_levels.txt");
     let file_path = path.canonicalize().unwrap();
     debug!("Reading from {:?}", file_path);
 
@@ -32,16 +31,20 @@ fn generate_file(words: &Vec<AWord>){
 
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum PartOfSpeech {
     Verb,
     Noun,
     Adjective,
     Adverb,
     Preposition,
-    Conjuction,
+    Conjunction,
     Pronoun,
     Number,
+    Determiner,
+    Exclamation,
+    Article,
+    Unknown,
 }
 
 #[derive(Debug, PartialEq)]
@@ -58,149 +61,76 @@ enum LangLevel {
 struct AWord {
     word: String,
     level: LangLevel,
-    pos: Vec<PartOfSpeech>,
+    pos: PartOfSpeech,
 }
 
 fn parse_file(contents: &String) -> Vec<AWord> {
     let mut words = Vec::new();
-    for line in contents.split("\n") {
+    let mut content_iter = contents.split("\n");
+    while let (Some(line1), Some(line2)) = (content_iter.next(), content_iter.next()) {
         let (
             word,
             part_of_speech,
             level,
         ) = {
             let mut word: String = String::new();
-            let mut part_of_speech: Vec<PartOfSpeech> = Vec::new();
-            let mut level: LangLevel = LangLevel::Unknown;
-            for part in line.trim().split(" ").collect::<Vec<&str>>().iter().rev() {
+            let mut part_of_speech: PartOfSpeech = PartOfSpeech::Unknown;
+            let level: LangLevel = match line2.trim() {
+                "a1" => {
+                    LangLevel::A1
+                },
+                "a2" => {
+                    LangLevel::A2
+                },
+                "b1" => {
+                    LangLevel::B1
+                },
+                "b2" => {
+                    LangLevel::B2
+                },
+                "c1" => {
+                    LangLevel::C1
+                },
+                "c2" => {
+                    LangLevel::C2
+                },
+                _ => LangLevel::Unknown
+            };
+            for part in line1.trim().split(" ").collect::<Vec<&str>>().iter().rev() {
                 match (*part).trim_end_matches(',') {
-                    "A1" => {
-                        level = LangLevel::A1
-                    },
-                    "A2" => {
-                        level = LangLevel::A2
-                    },
-                    "B1" => {
-                        level = LangLevel::B1
-                    },
-                    "B2" => {
-                        level = LangLevel::B2
-                    },
-                    "C1" => {
-                        level = LangLevel::C1
-                    },
-                    "C2" => {
-                        level = LangLevel::C2
-                    },
-                    "v." => {
-                        part_of_speech.push(PartOfSpeech::Verb)
-                    },
-                    "n." => {
-                        part_of_speech.push(PartOfSpeech::Noun)
-                    },
-                    "adj." => {
-                        part_of_speech.push(PartOfSpeech::Adjective)
-                    },
-                    "adv." => {
-                        part_of_speech.push(PartOfSpeech::Adverb)
-                    },
-                    "prep." => {
-                        part_of_speech.push(PartOfSpeech::Preposition)
-                    },
-                    "conj." => {
-                        part_of_speech.push(PartOfSpeech::Conjuction)
-                    },
-                    "pron." => {
-                        part_of_speech.push(PartOfSpeech::Pronoun)
-                    },
-                    "number" => {
-                        part_of_speech.push(PartOfSpeech::Number);
-                        part_of_speech.push(PartOfSpeech::Noun);
-                        part_of_speech.push(PartOfSpeech::Adjective);
-                    },
-                    x => {
-                        word = x.to_string()
-                    }
-                }
-            }
-            (
-                word,
-                part_of_speech,
-                level,
-            )
-        };
-        if level == LangLevel::Unknown
-            || part_of_speech.len() == 0 {
-            println!(
-                "Word: {:?}, POS: {:?}, Level: {:?}", word, part_of_speech, level
-            );
-            continue;
-        }
-        words.push(AWord {
-            word,
-            pos: part_of_speech,
-            level,
-        });
-    }
-    words
-}
 
-fn parse_file_old(contents: &String) -> Vec<AWord> {
-    let mut words = Vec::new();
-    for line in contents.split("\n") {
-        let (
-            word,
-            part_of_speech,
-            level,
-        ) = {
-            let mut word: String = String::new();
-            let mut part_of_speech: Vec<PartOfSpeech> = Vec::new();
-            let mut level: LangLevel = LangLevel::Unknown;
-            for part in line.trim().split(" ").collect::<Vec<&str>>().iter().rev() {
-                match (*part).trim_end_matches(',') {
-                    "A1" => {
-                        level = LangLevel::A1
+                    "verb" => {
+                        part_of_speech = PartOfSpeech::Verb
                     },
-                    "A2" => {
-                        level = LangLevel::A2
+                    "noun" => {
+                        part_of_speech = PartOfSpeech::Noun
                     },
-                    "B1" => {
-                        level = LangLevel::B1
+                    "adjective" => {
+                        part_of_speech = PartOfSpeech::Adjective
                     },
-                    "B2" => {
-                        level = LangLevel::B2
+                    "adverb" => {
+                        part_of_speech = PartOfSpeech::Adverb
                     },
-                    "C1" => {
-                        level = LangLevel::C1
+                    "preposition" => {
+                        part_of_speech = PartOfSpeech::Preposition
                     },
-                    "C2" => {
-                        level = LangLevel::C2
+                    "conjunction" => {
+                        part_of_speech = PartOfSpeech::Conjunction
                     },
-                    "v." => {
-                        part_of_speech.push(PartOfSpeech::Verb)
-                    },
-                    "n." => {
-                        part_of_speech.push(PartOfSpeech::Noun)
-                    },
-                    "adj." => {
-                        part_of_speech.push(PartOfSpeech::Adjective)
-                    },
-                    "adv." => {
-                        part_of_speech.push(PartOfSpeech::Adverb)
-                    },
-                    "prep." => {
-                        part_of_speech.push(PartOfSpeech::Preposition)
-                    },
-                    "conj." => {
-                        part_of_speech.push(PartOfSpeech::Conjuction)
-                    },
-                    "pron." => {
-                        part_of_speech.push(PartOfSpeech::Pronoun)
+                    "pronoun" => {
+                        part_of_speech = PartOfSpeech::Pronoun
                     },
                     "number" => {
-                        part_of_speech.push(PartOfSpeech::Number);
-                        part_of_speech.push(PartOfSpeech::Noun);
-                        part_of_speech.push(PartOfSpeech::Adjective);
+                        part_of_speech = PartOfSpeech::Number
+                    },
+                    "determiner" => {
+                        part_of_speech = PartOfSpeech::Determiner
+                    },
+                    "exclamation" => {
+                        part_of_speech = PartOfSpeech::Exclamation
+                    },
+                    "article" => {
+                        part_of_speech = PartOfSpeech::Article
                     },
                     x => {
                         word = x.to_string()
@@ -214,7 +144,7 @@ fn parse_file_old(contents: &String) -> Vec<AWord> {
             )
         };
         if level == LangLevel::Unknown
-            || part_of_speech.len() == 0 {
+            || part_of_speech == PartOfSpeech::Unknown {
             println!(
                 "Word: {:?}, POS: {:?}, Level: {:?}", word, part_of_speech, level
             );
