72ffab291743e8203b97193b6622832a08d102eb
Ertugrul Karademir <ertugrul.karademir@oliverwyman.com>
2020-10-26 17:41:08 +0000

    Decided not to go with macros this time?

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -2,30 +2,38 @@
 // Playground
 // ############################################################################
 
-use petgraph::{self, visit};
+mod inner{
+    pub enum ATask {
+
+    }
+    #[macro_export]
+    macro_rules! tasks {
+        (
+            $(
+                mapper $mapper:ident() $mapper_expr: block
+            )*
+            $(
+                reducer $reducer:ident() $reducer_expr: block
+            )*
+        ) => {
+            $(
+                fn $reducer() $reducer_expr
+            )*
+        };
+    }
+}
+
+tasks!(
+    reducer osman() {
+        println!("OSMAN");
+    }
+
+    reducer orhan() {
+        println!("ORHAN");
+    }
+);
 
 fn main() {
-    let mut my_graph: petgraph::Graph<&str, (), petgraph::Directed> = petgraph::Graph::new();
-
-    let el_0 = my_graph.add_node("level_0");
-    let el_1_0 = my_graph.add_node("level_1_0");
-    let el_1_1 = my_graph.add_node("level_1_1");
-    let edge_01_0 = my_graph.add_edge(el_0, el_1_0, ());
-    let edge_01_1 = my_graph.add_edge(el_0, el_1_1, ());
-    // let cycle = my_graph.add_edge(el_1_0, el_0, ());
-    let el_2 = my_graph.add_node("level_2");
-    let edge_21_0 = my_graph.add_edge(el_1_0, el_2, ());
-    let edge_21_1 = my_graph.add_edge(el_1_1, el_2, ());
-
-    println!("{:?}", my_graph);
-
-    // let mut bfs = visit::Bfs::new(&my_graph, el_0);
-    // while let Some(node_idx) = bfs.next(&my_graph) {
-    //     println!("{:?}", my_graph[node_idx]);
-    // }
-
-    let mut topo = visit::Topo::new(&my_graph);
-    while let Some(node_idx) = topo.next(&my_graph) {
-        println!("{:?}", my_graph[node_idx]);
-    }
+    osman();
+    orhan();
 }
