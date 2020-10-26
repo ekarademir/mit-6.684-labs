e978e7a92a80e70a892fb0668cdbe0618c7340dd
Ertugrul Karademir <ertugrul.karademir@oliverwyman.com>
2020-10-26 15:43:29 +0000

    Implemented task pipeline

--- map-reduce/src/tasks/pipeline.rs
+++ map-reduce/src/tasks/pipeline.rs
@@ -59,9 +59,61 @@ mod tests {
 
     task_pipeline.add_order(count_words, sum_counts);
 
+    // count_words -> sum_counts
+
+
     let expected = vec![
       super::ATask::CountWords,
       super::ATask::SumCounts
+    ];
+
+    for (idx, task) in task_pipeline.into_iter().enumerate() {
+      assert_eq!(expected[idx], task);
+    }
+  }
+
+  #[test]
+  #[cfg_attr(feature = "dont_test_this", ignore)]
+  fn test_cyclic() {
+    let mut task_pipeline = super::Pipeline::new();
+    let count_words = task_pipeline.add_task(super::ATask::CountWords);
+    let sum_counts = task_pipeline.add_task(super::ATask::SumCounts);
+
+    task_pipeline.add_order(count_words, sum_counts);
+    task_pipeline.add_order(sum_counts, count_words);
+
+    // count_words -> sum_counts
+    //            \__/
+
+    let expected: Vec<super::ATask> = Vec::new();
+
+    for (idx, task) in task_pipeline.into_iter().enumerate() {
+      assert_eq!(expected[idx], task);
+    }
+  }
+
+  #[test]
+  #[cfg_attr(feature = "dont_test_this", ignore)]
+  fn test_multi_level_dag() {
+    let mut task_pipeline = super::Pipeline::new();
+    let count_words1 = task_pipeline.add_task(super::ATask::CountWords);
+    let sum_counts1 = task_pipeline.add_task(super::ATask::SumCounts);
+    let count_words2 = task_pipeline.add_task(super::ATask::CountWords);
+    let sum_counts2 = task_pipeline.add_task(super::ATask::SumCounts);
+
+    task_pipeline.add_order(sum_counts1, sum_counts2);
+    task_pipeline.add_order(count_words1, sum_counts2);
+    task_pipeline.add_order(sum_counts2, count_words2);
+
+    // sum_counts1 \
+    //              |-> sum_counts2 -> count_words2
+    // count_words1/
+
+    let expected = vec![
+      super::ATask::SumCounts,
+      super::ATask::CountWords,
+      super::ATask::SumCounts,
+      super::ATask::CountWords,
     ];
 
     for (idx, task) in task_pipeline.into_iter().enumerate() {
