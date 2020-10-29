7a47cf4be5d7cdbca0e56ecd8483954f9832fb21
Ertugrul Karademir <ekarademir@gmail.com>
2020-10-29 00:22:05 +0000

    Playing with graph traversal

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -7,6 +7,12 @@ fn main() {
     let mut myg: petgraph::Graph<&str, (), petgraph::Directed> = petgraph::Graph::new();
     let node1 = myg.add_node("node1");
     let node2 = myg.add_node("node2");
+    let node3 = myg.add_node("node3");
+    myg.add_edge(node1, node2, ());
+    myg.add_edge(node1, node3, ());
 
-    println!("Node 1: {:?}", myg[node1]);
+    println!("Node 1 {:?}", node1);
+    for neighbor in myg.neighbors_directed(node1, petgraph::Direction::Outgoing) {
+        println!("{:?}", myg[neighbor]);
+    }
 }
