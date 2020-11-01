d0de4ea266fe059032210130dcbac74cc644334b
Ertugrul Karademir <ekarademir@gmail.com>
2020-11-01 21:16:01 +0000

    Playing with array comp

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,18 +1,7 @@
 // ############################################################################
 // Playground
 // ############################################################################
-use petgraph;
 
 fn main() {
-    let mut myg: petgraph::Graph<&str, (), petgraph::Directed> = petgraph::Graph::new();
-    let node1 = myg.add_node("node1");
-    let node2 = myg.add_node("node2");
-    let node3 = myg.add_node("node3");
-    myg.add_edge(node1, node2, ());
-    myg.add_edge(node1, node3, ());
-
-    println!("Node 1 {:?}", node1);
-    for neighbor in myg.neighbors_directed(node1, petgraph::Direction::Outgoing) {
-        println!("{:?}", myg[neighbor]);
-    }
+    let a = [1, 2];
 }
